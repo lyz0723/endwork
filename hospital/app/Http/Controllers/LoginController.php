fc5e6a8184610381c80be71c75c6a312faadf6ca
lyz <360195045@qq.com>
2016-08-04 10:26:52 +0800

    2.1

--- hospital/app/Http/Controllers/LoginController.php
+++ hospital/app/Http/Controllers/LoginController.php
@@ -57,6 +57,29 @@ class LoginController extends Controller
     }
 
 
+    public function lianjie(){
+        // 1.将timestamp， nonce，token 按字典排序
+        $timestamp =$_GET['timestamp'];
+        $nonce     = $_GET['nonce'];
+        $token     = 'weixin';
+        $signature = $_GET['signature'];
+        $array     = array( $timestamp, $nonce, $token );
+        sort( $array );
+        // 2.将排序后的三个参数拼接之后用sha1加密
+        $tmpstr    = implode('', $array);
+        $tmpstr    = sha1( $tmpstr );
+        // 3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
+        $echoster=$_GET['echostr'];
+        if ( $tmpstr == $signature && $echoster) {
+            echo $_GET['echostr'];
+            exit;
+        }else {
+            $this -> getSignPackage();
+        }
+    }
+
+
+
     //获取Access_token
     public function getWxAccessToken(){
         $appid="wx9036c924e93284c6";
