988c5bd5b56c2073911f42473157f0d10607bc1e
lyz <360195045@qq.com>
2016-08-04 15:01:58 +0800

    4.7

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -64,10 +64,10 @@
             type: '', // 分享类型,music、video或link，不填默认为link
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function () {
-                // 用户确认分享后执行的回调函数
+                alert('已分享');
             },
             cancel: function () {
-                // 用户取消分享后执行的回调函数
+                alert('已取消');
             }
         });
     });
