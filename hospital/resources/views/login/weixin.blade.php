1a80ea460114e5f31bdf6f19362a62473c2a1d3d
lyz <360195045@qq.com>
2016-08-04 09:56:50 +0800

    1.8

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -25,15 +25,42 @@
         ]
     });
     wx.ready(function () {
-        wx.onMenuShareTimeline({
-            title: '', // 分享标题
-            link: '', // 分享链接
-            imgUrl: '', // 分享图标
-            success: function () {
-                // 用户确认分享后执行的回调函数
+        wx.onMenuShareAppMessage({
+            title: '<?php echo $news['Title'];?>',
+            desc: '<?php echo $news['Description'];?>',
+            link: '<?php echo $news['Url'];?>',
+            imgUrl: '<?php echo $news['PicUrl'];?>',
+            trigger: function (res) {
+                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                // alert('用户点击发送给朋友');
             },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
+            success: function (res) {
+                // alert('已分享');
+            },
+            cancel: function (res) {
+                // alert('已取消');
+            },
+            fail: function (res) {
+                // alert(JSON.stringify(res));
+            }
+        });
+
+        wx.onMenuShareTimeline({
+            title: '<?php echo $news['Title'];?>',
+            link: '<?php echo $news['Url'];?>',
+            imgUrl: '<?php echo $news['PicUrl'];?>',
+            trigger: function (res) {
+                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                // alert('用户点击分享到朋友圈');
+            },
+            success: function (res) {
+                // alert('已分享');
+            },
+            cancel: function (res) {
+                // alert('已取消');
+            },
+            fail: function (res) {
+                // alert(JSON.stringify(res));
             }
         });
     });
