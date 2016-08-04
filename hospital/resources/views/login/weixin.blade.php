9381ee7761dbd5075d5c334c58f4c2a7adc4a693
lyz <360195045@qq.com>
2016-08-04 15:24:51 +0800

    4.9

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -8,31 +8,20 @@
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
     wx.config({
-        debug: false,
+        debug: true,
         appId: '<?php echo $signPackage['appId'];?>',
         timestamp: '<?php echo $signPackage["timestamp"];?>',
         nonceStr: '<?php echo $signPackage["nonceStr"];?>',
         signature: '<?php echo $signPackage["signature"];?>',
         jsApiList: [
             // 所有要调用的 API 都要加到这个列表中
-            'checkJsApi',
-            'getLocation',
             'onMenuShareTimeline',
-            'onMenuShareAppMessage'
+            'onMenuShareAppMessage',
+            'chooseImage'
         ]
     });
-    
+
     wx.ready(function () {
-        wx.getLocation({
-            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-            success: function (res) {
-                alert(1);
-                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                var speed = res.speed; // 速度，以米/每秒计
-                var accuracy = res.accuracy; // 位置精度
-            }
-        });
         //享到朋友圈
         wx.onMenuShareTimeline({
             title: '<?php echo $news['Title'];?>',
@@ -58,6 +47,15 @@
             },
             cancel: function () {
                 alert('已取消');
+            }
+        });
+        //拍照或从手机相册中选图接口
+        wx.chooseImage({
+            count: 1, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
             }
         });
     });
