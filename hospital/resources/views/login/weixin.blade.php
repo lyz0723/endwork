6be9bca2b92ab3004f44aab1eceb9b5b42bc91a7
lyz <360195045@qq.com>
2016-08-04 13:59:06 +0800

    3.4

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -28,7 +28,6 @@
     wx.checkJsApi({
         jsApiList: [
             'getLocation',
-            'onMenuShareTimeline',
             'onMenuShareAppMessage'
         ],
         success: function (res) {
@@ -44,25 +43,32 @@
                 var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                 var speed = res.speed; // 速度，以米/每秒计
                 var accuracy = res.accuracy; // 位置精度
+                _shareAppMessage();
             }
         });
 
-        
-
     });
 
+    function _shareAppMessage() {
+        // 页面加载后设置微信分享给朋友的内容
+        wx.onMenuShareAppMessage({
+            title: '圣诞老人送礼啦，现金红包人人领！', // 分享标题
+            desc: '“圣诞夺包”35000份礼包等你拆！', // 分享描述
+            link: encodeURI(curDomain + '/christmas/service/ChristmasSockOnline.home.do?area=cd'),//encodeURI(window.location.href.replace('&from=ad', '')), // 分享链接
+            imgUrl: url + '/public/christmas/img/shorejoin.jpg', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                //Message.toast.success("分享成功！").appear();
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                //alert('cancel');
+            }
+        });
+    }
 
-    wx.onMenuShareTimeline({
-        title: '<?php echo $news['Title'];?>',
-        link: '<?php echo $news['Url'];?>',
-        imgUrl: '<?php echo $news['PicUrl'];?>',
-        success: function (res) {
-            alert('已分享');
-        },
-        cancel: function (res) {
-            alert('已取消');
-        }
-    });
 
 </script>
 </body>
