e0f364d45bb4b5bdfb7459626dfe62152b9dbd7a
lyz <360195045@qq.com>
2016-08-04 10:38:46 +0800

    2.5

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -5,13 +5,12 @@
 
 </head>
 <body>
-
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-<script>
-    wx.config(
-            {<?php
+<?php
     print_r($news);
 ?>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+    wx.config({
         debug: false,
         appId: '<?php echo $signPackage['appId'];?>',
         timestamp: '<?php echo $signPackage["timestamp"];?>',
@@ -26,6 +25,16 @@
             'onMenuShareAppMessage'
         ]
     });
+    wx.checkJsApi({
+        jsApiList: [
+            'getLocation',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage'
+        ],
+        success: function (res) {
+            alert(JSON.stringify(res));
+        }
+    });
 
     wx.ready(function () {
         wx.getLocation({
@@ -38,58 +47,46 @@
             }
         });
 
-        wx.checkJsApi({
-            jsApiList: [
-                'getLocation',
-                'onMenuShareTimeline',
-                'onMenuShareAppMessage'
-            ],
-            success: function (res) {
-                alert(JSON.stringify(res));
-            }
-        });
-
-        wx.onMenuShareAppMessage({
-            title: '<?php echo $news['Title'];?>',
-            desc: '<?php echo $news['Description'];?>',
-            link: '<?php echo $news['Url'];?>',
-            imgUrl: '<?php echo $news['PicUrl'];?>',
-            trigger: function (res) {
-                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                // alert('用户点击发送给朋友');
-            },
-            success: function (res) {
-                // alert('已分享');
-            },
-            cancel: function (res) {
-                // alert('已取消');
-            },
-            fail: function (res) {
-                // alert(JSON.stringify(res));
-            }
-        });
-
-        wx.onMenuShareTimeline({
-            title: '<?php echo $news['Title'];?>',
-            link: '<?php echo $news['Url'];?>',
-            imgUrl: '<?php echo $news['PicUrl'];?>',
-            trigger: function (res) {
-                // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                // alert('用户点击分享到朋友圈');
-            },
-            success: function (res) {
-                // alert('已分享');
-            },
-            cancel: function (res) {
-                // alert('已取消');
-            },
-            fail: function (res) {
-                // alert(JSON.stringify(res));
-            }
-        });
     });
 
+    wx.onMenuShareAppMessage({
+        title: '<?php echo $news['Title'];?>',
+        desc: '<?php echo $news['Description'];?>',
+        link: '<?php echo $news['Url'];?>',
+        imgUrl: '<?php echo $news['PicUrl'];?>',
+        trigger: function (res) {
+            // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+            // alert('用户点击发送给朋友');
+        },
+        success: function (res) {
+            // alert('已分享');
+        },
+        cancel: function (res) {
+            // alert('已取消');
+        },
+        fail: function (res) {
+            // alert(JSON.stringify(res));
+        }
+    });
 
+    wx.onMenuShareTimeline({
+        title: '<?php echo $news['Title'];?>',
+        link: '<?php echo $news['Url'];?>',
+        imgUrl: '<?php echo $news['PicUrl'];?>',
+        trigger: function (res) {
+            // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+            // alert('用户点击分享到朋友圈');
+        },
+        success: function (res) {
+            // alert('已分享');
+        },
+        cancel: function (res) {
+            // alert('已取消');
+        },
+        fail: function (res) {
+            // alert(JSON.stringify(res));
+        }
+    });
 
 
 
