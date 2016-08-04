897141bd3a38892632bfc593046487dec838887e
lyz <360195045@qq.com>
2016-08-04 09:47:54 +0800

    1.6

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -6,22 +6,6 @@
 </head>
 <body>
 <?php echo $signPackage['appId']?>
-{{--<script>--}}
-    {{--wx.config({--}}
-        {{--debug: false,--}}
-        {{--appId: '<?php echo $signPackage->appId;?>',--}}
-        {{--timestamp: <?php echo $signPackage["timestamp"];?>,--}}
-        {{--nonceStr: '<?php echo $signPackage["nonceStr"];?>',--}}
-        {{--signature: '<?php echo $signPackage["signature"];?>',--}}
-        {{--jsApiList: [--}}
-            {{--// 所有要调用的 API 都要加到这个列表中--}}
-            {{--'checkJsApi',--}}
-            {{--'openLocation',--}}
-            {{--'getLocation',--}}
-            {{--'onMenuShareTimeline',--}}
-            {{--'onMenuShareAppMessage'--}}
-        {{--]--}}
-    {{--});--}}
-{{--</script>--}}
+
 </body>
 </html>
