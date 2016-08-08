d655a4e8f95b57fada122747a2eb46fd64281b72
lyz <360195045@qq.com>
2016-08-08 10:20:52 +0800

    1.0

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -15,9 +15,12 @@
         signature: '<?php echo $signPackage["signature"];?>',
         jsApiList: [
             // 所有要调用的 API 都要加到这个列表中
-            'onMenuShareTimeline',
-            'onMenuShareAppMessage',
-            'onMenuShareQZone'
+                 'onMenuShareTimeline',
+                'onMenuShareAppMessage',
+                'onMenuShareQZone',
+                'openLocation',
+                'getLocation'
+
         ]
     });
 
@@ -60,6 +63,25 @@
             },
             cancel: function () {
                 alert('已取消');
+            }
+        });
+        //使用微信内置地图查看位置接口
+        wx.openLocation({
+            latitude: 0, // 纬度，浮点数，范围为90 ~ -90
+            longitude: 0, // 经度，浮点数，范围为180 ~ -180。
+            name: '北京八维研修学院', // 位置名
+            address: '北京市海淀区软件园南', // 地址详情说明
+            scale: 2, // 地图缩放级别,整形值,范围从1~28。默认为最大
+            infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+        });
+        //获取地理位置接口
+        wx.getLocation({
+            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+            success: function (res) {
+                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                var speed = res.speed; // 速度，以米/每秒计
+                var accuracy = res.accuracy; // 位置精度
             }
         });
     });
