c5e92e0400c4a5cd279b0159d827179809a4500a
lyz <360195045@qq.com>
2016-08-08 10:40:32 +0800

    1.3

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -5,7 +5,7 @@
 
 </head>
 <body>
-<script src="{{URL::asset('/')}}jquery.js"></script>
+<script src="{{URL::asset('/')}}js/jquery.js"></script>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <a id="get-location" href="javascript:;">获取地址</a>
 <script>
@@ -71,7 +71,7 @@
 
 
     $("#get-location").click(function(){
-        alert(1)
+
     });
 
 
