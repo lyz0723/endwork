035ea36686ea4cc674a3521265364e52a48b8ae6
lyz <360195045@qq.com>
2016-08-08 10:48:09 +0800

    1.7

--- hospital/resources/views/login/weixin.blade.php
+++ hospital/resources/views/login/weixin.blade.php
@@ -80,16 +80,16 @@
                 var speed = res.speed; // 速度，以米/每秒计
                 var accuracy = res.accuracy; // 位置精度
 
+                //使用微信内置地图查看位置接口
+                wx.openLocation({
+                    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+                    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+                    name: '北京八维研修学院', // 位置名
+                    address: '北京市海淀区软件园南', // 地址详情说明
+                    scale: 2, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                    infoUrl: 'http://www.baidu.com' // 在查看位置界面底部显示的超链接,可点击跳转
+                });
             }
-        });
-        //使用微信内置地图查看位置接口
-        wx.openLocation({
-            latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
-            longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
-            name: '北京八维研修学院', // 位置名
-            address: '北京市海淀区软件园南', // 地址详情说明
-            scale: 2, // 地图缩放级别,整形值,范围从1~28。默认为最大
-            infoUrl: 'http://www.baidu.com' // 在查看位置界面底部显示的超链接,可点击跳转
         });
     });
 
